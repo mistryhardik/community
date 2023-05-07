17af5da5daffcf57dfb6dea03e64c5580363a963
Hardik Mistry <mistryhardik.05@live.com>
2023-05-07 20:19:17 +0530

    [Refactor] Update sample and presentation

--- gab/mumbai 2023/chaos/chaos.app/Controllers/HealthController.cs
+++ gab/mumbai 2023/chaos/chaos.app/Controllers/HealthController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace gab.backend.Controllers
+namespace chaos.app.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
---
+++ gab/mumbai 2023/chaos/chaos.app/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using chaos.app.Models;
+
+namespace chaos.app.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    public IActionResult Index()
+    {
+        ViewData["WelcomeMessage"] = $"Welcome from {_configuration["App:Region"]}";
+        
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
